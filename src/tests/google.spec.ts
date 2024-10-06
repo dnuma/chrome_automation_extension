b2219b2861af9a0f9c2ffbe5a26cff4cdf2ae3f5
David Numa <david.numa@gmail.com>
2024-10-06 11:30:50 -0400

    google test started

--- src/tests/google.spec.ts
+++ src/tests/google.spec.ts
@@ -1,1 +1,27 @@
-//googgle test
+import { test, expect, request } from '@playwright/test';
+import { BasePage } from '../pages/base.page';
+
+test.beforeEach(async ({page}) => {
+  const title = "Google"
+  const url = "https://www.google.com/";
+  
+  await page.goto(url);
+  expect(page).toHaveTitle(title);
+});
+
+test.describe("Simple Google search and validation",
+  {
+    tag: "@google"
+  },
+  () => {
+    test("Search and asssert", async( { page }) => {
+      const basePage = new BasePage(page);
+
+      
+
+    })
+  }
+
+
+
+)
