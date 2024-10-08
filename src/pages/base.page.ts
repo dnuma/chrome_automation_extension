41ad8597122fad30d6a2bf6e589edbec1d1bb4e8
David Numa <david.numa@gmail.com>
2024-10-08 08:33:07 -0400

    Bing test created

--- src/pages/base.page.ts
+++ src/pages/base.page.ts
@@ -5,12 +5,16 @@ export class BasePage {
   readonly googleSearchBox: Locator;
   readonly googleSearchButton: Locator;
   readonly googleSearchResult: Locator;
+  readonly bingSearchBox: Locator;
+  readonly bingSearchResult: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.googleSearchBox = this.page.locator(`[title="Search"]`);
     this.googleSearchButton = this.page.locator(`input[aria-label="Google Search"]`).first();
     this.googleSearchResult = this.page.locator('h3').first();
+    this.bingSearchBox = this.page.locator(`#sb_form_q`);
+    this.bingSearchResult = this.page.locator(`ol li`).first();
   }
 
   /**
@@ -20,6 +24,16 @@ export class BasePage {
   async searchGoogle(textToSearch: string): Promise<void> {
     await this.googleSearchBox.fill(textToSearch);
     await this.googleSearchButton.click();
+  }
+
+  /**
+   * Searches in Bing
+   * @param {string} textToSearch - The text to be searched.
+   */
+  async searchBing(textToSearch: string): Promise<void> {
+    await this.page.waitForTimeout(1000); // Forced timeout for the ads to load
+    await this.bingSearchBox.fill(textToSearch);
+    await this.page.keyboard.press('Enter');
   }
 
 }
--- src/tests/bing.spec.ts
+++ src/tests/bing.spec.ts
@@ -1,1 +1,25 @@
-// bing tests
+import { test, expect } from '@playwright/test';
+import { BasePage } from '../pages/base.page';
+
+test.beforeEach(async ({page}) => {
+  const title = "Bing"
+  const url = "https://www.bing.com/";
+  
+  await page.goto(url);
+  expect(page).toHaveTitle(title);
+});
+
+test.describe("Simple Bing search and validation",
+  {
+    tag: "@bing"
+  },
+  () => {
+    test("Search and asssert", async( { page }) => {
+      const basePage = new BasePage(page);
+      const searchTerm = "Playwright";
+
+      await basePage.searchBing(searchTerm);
+      await expect(basePage.bingSearchResult).toContainText(searchTerm);     
+    })
+  }
+)
